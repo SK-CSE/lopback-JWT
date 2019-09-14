eab944819d838ba18617fda97ef46cbed98944f2
Saurabh Kumar <saurabh.cse04@gmail.com>
2019-09-14 14:42:24 +0530

    lb3 app

---
+++ common/models/message.js
@@ -0,0 +1,13 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-workspace
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+module.exports = function(Message) {
+  Message.greet = function(msg, cb) {
+    process.nextTick(function() {
+      msg = msg || 'hello';
+      cb(null, 'Sender says ' + msg + ' to receiver');
+    });
+  };
+};
